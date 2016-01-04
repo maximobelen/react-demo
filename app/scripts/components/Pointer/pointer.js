f526c09dd0879988ce61eee9a106da528af141f6
maximobelen <maximo@jam3.com>
2016-01-04 19:04:44 -0300

    add pointer stuff

--- app/scripts/components/Pointer/pointer.js
+++ app/scripts/components/Pointer/pointer.js
@@ -4,13 +4,12 @@ var ReactDOM = require('react-dom');
 var TweenMax = require('gsap');
 
 var Pointer = React.createClass({
-
-    render: function() {
-        return (
-            <div id="pointer">
-            </div>
-        );
-    }
+  render: function() {
+    return (
+        <div id="pointer">
+        </div>
+    );
+  }
 });
 
 module.exports = Pointer;
